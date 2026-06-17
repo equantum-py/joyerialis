e3c200b1bdbdc59ef1814ebe585ab34deb80d1d5
eQuantum <derlisa567@gmail.com>
2026-06-17 17:34:10 -0300

    feat: restore ecommerce home structure

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,28 +1,41 @@
 import SEO from "@/components/seo";
 import Wrapper from "@/layout/wrapper";
+
 import HeaderFour from '@/layout/headers/header-4';
 import JewelryBanner from '@/components/banner/jewelry-banner';
-import JewelryShopBanner from '@/components/shop-banner/jewelry-shop-banner';
-import JewelryAbout from '@/components/about/jewelry-about';
+import FeatureAreaThree from '@/components/features/feature-area-3';
+
+import InstagramAreaFour from '@/components/instagram/instagram-area-4';
 import PopularProducts from '@/components/products/jewelry/popular-products';
 import ProductArea from '@/components/products/jewelry/product-area';
-import InstagramAreaFour from '@/components/instagram/instagram-area-4';
-import FeatureAreaThree from '@/components/features/feature-area-3';
+
+import JewelryCollectionBanner from '@/components/shop-banner/jewelry-collection-banner';
+import BestSellerPrd from '@/components/products/jewelry/best-seller-prd';
+
 import FooterTwo from '@/layout/footers/footer-2';
 
 export default function Home() {
   return (
     <Wrapper>
-      <SEO pageTitle='Home'/>
-      <HeaderFour/>
-      <JewelryBanner/>
+      <SEO pageTitle="Joyerialis" />
+
+      <HeaderFour />
+
+      <JewelryBanner />
+
       <FeatureAreaThree />
-      <JewelryShopBanner/>
-      <JewelryAbout/>
-      <PopularProducts/>
-      <ProductArea/>
-      <InstagramAreaFour/>
-      <FooterTwo/>
+
+      <InstagramAreaFour />
+
+      <PopularProducts />
+
+      <ProductArea />
+
+      <JewelryCollectionBanner />
+
+      <BestSellerPrd />
+
+      <FooterTwo />
     </Wrapper>
-  )
+  );
 }

459bf8e88011ffcc1a2a6eaea17678b40e83b67b
eQuantum <derlisa567@gmail.com>
2026-06-18 11:22:19 -0300

    feat: replace instagram gallery with category navigation

--- src/components/instagram/instagram-area-4.jsx
+++ src/components/instagram/instagram-area-4.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 // internal
 import insta_1 from '@assets/img/instagram/4/instagram-1.jpg';
 import insta_2 from '@assets/img/instagram/4/instagram-2.jpg';
@@ -8,38 +9,45 @@ import insta_4 from '@assets/img/instagram/4/instagram-4.jpg';
 import insta_5 from '@assets/img/instagram/4/instagram-5.jpg';
 import insta_6 from '@assets/img/instagram/4/instagram-6.jpg';
 
-// instagram data 
-const instagram_data = [
-  { id: 1, link: 'https://www.instagram.com/', img: insta_1 },
-  { id: 2, link: 'https://www.instagram.com/', img: insta_2 },
-  { id: 3, link: 'https://www.instagram.com/', img: insta_3 },
-  { id: 4, link: 'https://www.instagram.com/', img: insta_4 },
-  { id: 5, link: 'https://www.instagram.com/', img: insta_5 },
-  { id: 6, link: 'https://www.instagram.com/', img: insta_6 },
-]
+const category_data = [
+  { id: 1, name: 'Aretes',    link: '/shop?category=earrings',  img: insta_1 },
+  { id: 2, name: 'Anillos',   link: '/shop?category=rings',     img: insta_2 },
+  { id: 3, name: 'Collares',  link: '/shop?category=necklaces', img: insta_3 },
+  { id: 4, name: 'Pulseras',  link: '/shop?category=bracelets', img: insta_4 },
+  { id: 5, name: 'Conjuntos', link: '/shop',                    img: insta_5 },
+  { id: 6, name: 'Medallas',  link: '/shop?category=medals',    img: insta_6 },
+];
 
 const InstagramAreaFour = () => {
   return (
-    <>
-      <section className="tp-instagram-area tp-instagram-style-4 pb-10">
-        <div className="container-fluid pl-20 pr-20">
-          <div className="row row-cols-lg-6 row-cols-sm-2 row-cols-1 gx-2 gy-2 gy-lg-0">
-            {instagram_data.map((item, i) => (
-              <div className="col" key={i}>
-                <div className="tp-instagram-item-2 w-img">
-                  <Image src={item.img} alt="imagen de instagram" style={{ width: '100%', height: '100%' }} />
-                  <div className="tp-instagram-icon-2">
-                    <a href={item.link} target="_blank" className="popup-image">
-                      <i className="fa-brands fa-instagram"></i>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            ))}
+    <section className="tp-instagram-area tp-instagram-style-4 pt-60 pb-10">
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-12">
+            <div className="tp-section-title-wrapper-4 text-center mb-40">
+              <h3 className="tp-section-title-4">Comprar por categoría</h3>
+              <p>Encuentra la joya perfecta para cada ocasión.</p>
+            </div>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+      <div className="container-fluid pl-20 pr-20">
+        <div className="row row-cols-lg-6 row-cols-sm-2 row-cols-1 gx-2 gy-2 gy-lg-0">
+          {category_data.map((item) => (
+            <div className="col" key={item.id}>
+              <Link href={item.link}>
+                <div className="tp-instagram-item-2 tp-category-card w-img">
+                  <Image src={item.img} alt={item.name} style={{ width: '100%', height: '100%' }} />
+                  <div className="tp-category-label">
+                    <span>{item.name}</span>
+                  </div>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
   );
 };
 
